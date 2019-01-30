165aa49f128bc19704777344d163abcc37c986c3
kishore Reddy <vvkishorereddy@gmail.com>
2019-01-30 22:31:46 +0530

    modifying

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Home from "./components/home";
+import Contact from "./components/contact";
+import Faq from "./components/faq";
+import Jobs from "./components/jobs";
+import Blog from "./components/blog";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header" />
-      </div>
+      <React.Fragment>
+        <Header />
+        <Home />
+        <Contact />
+        <Jobs />
+        <Faq />
+        <Blog />
+        <Footer />
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/jobs/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Jobs extends Component {
+  render() {
+    return <div>Jobs</div>;
+  }
+}
