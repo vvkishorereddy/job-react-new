38d68e05132cedf36e28a67ea17ddadeb61d5777
kishore reddy <vvkishorereddy@gmail.com>
2019-01-31 16:47:52 +0530

    integrating

--- src/components/faq/index.js
+++ src/components/faq/index.js
@@ -1,7 +1,64 @@
 import React, { Component } from "react";
 import Banner from "./Banner";
 
+const data = [
+  {
+    id: 1,
+    title: "Aenean viverra rhoncus?",
+    content:
+      "Dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh etjusto cursus id rutrum lorem imperdiet."
+  },
+  {
+    id: 2,
+    title: "Pellentesque habitant morbi tristique?",
+    content:
+      "Dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh etjusto cursus id rutrum lorem imperdiet."
+  },
+  {
+    id: 3,
+    title: "Aenean viverra rhoncus?",
+    content:
+      "Dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh etjusto cursus id rutrum lorem imperdiet."
+  },
+  {
+    id: 4,
+    title: "Pellentesque habitant morbi tristique?",
+    content:
+      "Dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh etjusto cursus id rutrum lorem imperdiet."
+  },
+  {
+    id: 5,
+    title: "Aenean viverra rhoncus?",
+    content:
+      "Dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh etjusto cursus id rutrum lorem imperdiet."
+  },
+  {
+    id: 6,
+    title: "Pellentesque habitant morbi tristique?",
+    content:
+      "Dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh etjusto cursus id rutrum lorem imperdiet."
+  },
+  {
+    id: 7,
+    title: "Aenean viverra rhoncus?",
+    content:
+      "Dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh etjusto cursus id rutrum lorem imperdiet."
+  },
+  {
+    id: 8,
+    title: "Pellentesque habitant morbi tristique?",
+    content:
+      "Dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh etjusto cursus id rutrum lorem imperdiet."
+  }
+];
+
 export default class Faq extends Component {
+  state = {
+    data: []
+  };
+  componentDidMount() {
+    this.setState({ data: data });
+  }
   render() {
     return (
       <React.Fragment>
@@ -13,82 +70,18 @@ export default class Faq extends Component {
             </div>
             <div>
               <div className="w-row">
-                <div className="w-col w-col-6">
-                  <div>
-                    <div>
-                      <h3 className="faq-title">Aenean viverra rhoncus?</h3>
-                      <p>
-                        Dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        varius enim in eros elementum tristique. Duis cursus, mi
-                        quis viverra ornare, eros dolor interdum nulla, ut
-                        commodo diam libero vitae erat. Aenean faucibus nibh et
-                        justo cursus id rutrum lorem imperdiet.
-                      </p>
-                    </div>
-                    <div className="space" />
-                    <div>
-                      <h3 className="faq-title">
-                        Pellentesque habitant morbi tristique?
-                      </h3>
-                      <p>
-                        Suspendisse varius enim in eros elementum tristique.
-                        Duis cursus, mi quis viverra ornare, eros dolor interdum
-                        nulla, ut commodo diam libero vitae erat.
-                      </p>
-                    </div>
-                    <div className="space" />
-                    <div>
-                      <h3 className="faq-title">Phasellus gravida semper?</h3>
-                      <p>
-                        Consectetur adipiscing elit. Suspendisse varius enim in
-                        eros elementum tristique. Duis cursus, mi quis viverra
-                        orna interdum nulla.Aenean faucibus nibh et justo cursus
-                        id rutrum lorem imperdiet. Nunc ut sem vitae risus
-                        tristique posuere.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="w-col w-col-6">
-                  <div>
-                    <div>
-                      <div>
-                        <h3 className="faq-title">
-                          Pellentesque dapibus hendrerit tortor?
-                        </h3>
-                        <p>
-                          Ut leo. Vestibulum ante ipsum primis in faucibus orci
-                          luctus et ultrices posuere cubilia Curae; Sed aliquam,
-                          nisi quis porttitor congue, elit erat euismod orci, ac
-                          placerat dolor lectus quis orci. Praesent nonummy mi
-                          in odio. Nam quam nunc, blandit vel, luctus pulvinar,
-                          hendrerit id, lorem.
-                        </p>
-                      </div>
-                      <div className="space" />
-                      <div>
-                        <h3 className="faq-title">
-                          Aenean commodo ligula eget dolor?
-                        </h3>
-                        <p>
-                          Sit amet, consectetur adipiscing elit. Suspendisse
-                          varius enim in eros elementum tristique. Duis cursus,
-                          mi quis viverra ornare, eros dolor interdum nulla.
-                        </p>
-                      </div>
-                      <div className="space" />
-                      <div>
-                        <h3 className="faq-title">
-                          Vestibulum purus quam scelerisque?
-                        </h3>
-                        <p>
-                          Suspendisse varius enim in eros elementum tristique.
-                          Duis cursus, mi quis viverra ornare, eros dolor
-                          interdum nulla.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
+                <div className="w-col w-col-12">
+                  {this.state.data.map(post => {
+                    return (
+                      <React.Fragment>
+                        <div>
+                          <h3 className="faq-title">{post.title}</h3>
+                          <p>{post.content}</p>
+                        </div>
+                        <div className="space" />
+                      </React.Fragment>
+                    );
+                  })}
                 </div>
               </div>
             </div>
